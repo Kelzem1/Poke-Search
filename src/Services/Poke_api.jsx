ecb86d5ce27789fc12f756d5c96d292224e657f9
Rodolfo Rolon <kelzemrod@gmail.com>
2023-11-10 23:14:27 +0100

    Axios, getting api

---
+++ src/Services/Poke_api.jsx
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+
+
+const apiUrl = 'https://pokeapi.co/api/v2/pokemon/ditto'
+
+const PokeApi = () =>{
+
+    const [post, setPost] = useState(null)
+    useEffect(() =>{
+        axios.get(apiUrl).then((responde) =>{
+            setPost(response.data)
+        })
+    }, [])
+    if (!post) return null
+    return(
+        <div>
+            <h1></h1>
+            <p></p>
+        </div>
+    )
+}
+
+export default PokeApi
