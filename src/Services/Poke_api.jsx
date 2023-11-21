7fa46df26aea6e2c864f4903f731938f3c2abb97
Rodolfo Rolon <kelzemrod@gmail.com>
2023-11-21 22:23:51 +0100

    Img name and id

--- src/Services/Poke_api.jsx
+++ src/Services/Poke_api.jsx
@@ -13,18 +13,42 @@ const PokeApi = () =>{
 
     useEffect(() =>{
        const getPokemon = async () =>{
-            const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100&offset=0')
+            const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=20&offset=0')
             const listaPoke = await response.json()
             const { results } = listaPoke
-            console.log(listaPoke)
-            setPokeName(results)
+
+            const newPokemones = results.map( async (pokemon) => {
+                const response = await fetch(pokemon.url)
+                const poke = await response.json()
+
+                return {
+                    id: poke.id,
+                    name: poke.name,
+                    img: poke.sprites.other.dream_world.front_default
+                }
+            })
+            console.log(await (Promise.all(newPokemones)))
+
+            setPokeName(await (Promise.all(newPokemones)))
        }
        getPokemon()
     }, [])
     
     return(
         <div>
-            {pokeName.map(pokemon => <p>{pokemon.name}</p>)}
+            <div>
+                {
+                    pokeName.map(pokemon =>{
+                        return(
+                            <div>
+                                <img src={pokemon.img} alt={pokemon.name} />
+                                <p>{pokemon.name}</p>
+                                <p>{pokemon.id}</p>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </div>
         
     )
