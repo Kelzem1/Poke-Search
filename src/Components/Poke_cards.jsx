38618d6834a13040245e8af88dad1fbcd9ed847e
Rodolfo Rolon <kelzemrod@gmail.com>
2023-11-27 22:20:18 +0100

    Styles and png change

--- src/Components/Poke_cards.jsx
+++ src/Components/Poke_cards.jsx
@@ -1,14 +1,14 @@
 const PokeCard = ({title, id, img, name, tipo, height, weight, abilities, hp, attack, defense, specialAttack, specialDefense, speed}) =>{
     return(
-        <div className="flex-col justify-center align-middle bg-green-600 opacity-90 w-72 h-full rounded-sm">
+        <div className="flex-col justify-center align-middle bg-card-black opacity-90 w-72 h-full rounded">
             <div className="flex-col m-10 justify-center align-middle">
                 <span>#{id}</span>
                 <img className="w-76 h-16 m-auto flex justify-center align-middle" src={img} alt={id} width={100} height={100}></img>
-                <h1 className="font-bold pt-4 capitalize flex justify-center align-middle">{name}</h1>
-                <span className="black">Type: {tipo}</span>
+                <h1 className=" text-yellow-100 font-bold pt-4 capitalize flex justify-center align-middle">{name}</h1>
+                <span className="text-yellow-50">Type: {tipo}</span>
             </div>
 
-            <div className="flex-col flex-nowrap">
+            <div className="text-yellow-50 p-3 flex-col flex-nowrap">
                 <span className="p-1">Height: {height}</span>
                 <span className="p-1">Weight: {weight}</span>
                 <span className="p-1">Ability: {abilities}</span>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'card-black': '#474747'
+      },
+    },
   },
   plugins: [],
 }
