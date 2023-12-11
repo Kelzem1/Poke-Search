0d6e3ebf173b68c62a8e508c218b619e6436b43b
Rodolfo Rolon <kelzemrod@gmail.com>
2023-12-11 09:04:11 +0100

    many changes

--- src/Components/Poke_cards.jsx
+++ src/Components/Poke_cards.jsx
@@ -2,8 +2,8 @@
 
 const PokeCard = ({id, img, name, tipo, height, weight, abilities,}) =>{
     return(
-        <div className="justify-center align-middle bg-black w-full h-96 rounded-md backdrop-blur-md backdrop-filter bg-opacity-70 border-amber-300 border-2">
-            <div className="m-1 flex-col justify-center align-middle}">
+        <div className="justify-center align-middle bg-sky-950 w-full h-96 rounded-md backdrop-blur-md backdrop-filter bg-opacity-50 border-amber-300 border-2">
+            <div className="m-1 flex-col justify-center align-middle">
                 <span className="text-amber-200">#{id}</span>
                     <div className="shadow-xl shadow-slate-800 flex justify-center align-middle m-auto w-28 h-28 bg-gradient-to-r from-yellow-200 to-yellow-400 rounded-full opacity-80 hover:opacity-100">
                         <img className="w-16 transition-all hover:w-48" src={img} alt={id} width={200} height={200}></img>
@@ -13,10 +13,10 @@ const PokeCard = ({id, img, name, tipo, height, weight, abilities,}) =>{
                 <span className="text-yellow-100 capitalize p-3 font-bold">Type: {tipo}</span>
             </div>
 
-            <div className="text-yellow-50 p-3 grid">
-                <span className="p-1">Height: {height}</span>
-                <span className="p-1">Weight: {weight}</span>
-                <span className="p-1 capitalize">Ability: {abilities}</span>
+            <div className="text-yellow-50 p-3 flex-col">
+                <p className="p-2">Height: {height}</p>
+                <p className="p-2">Weight: {weight}</p>
+                <p className="p-2 capitalize">Ability: {abilities}</p>
             </div>
 
         </div>
--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,7 +1,10 @@
 const Search = () =>{
+
+    const onSubmit = ''
+
     const placeHolder = 'nombre, id, type.'
     return(
-        <div className="flex justify-center align-middle bg-slate-700 p-5">
+        <div className="flex justify-center align-middle bg-black p-5">
             <input className="bg-slate-800 w-3/6 p-4 h-12 rounded-md text-yellow-400 capitalize" placeholder={placeHolder}></input>
         </div>
     )
