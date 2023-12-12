43f8fc4332f9b93159289f35e4f4afff7b6009d6
Rodolfo Rolon <kelzemrod@gmail.com>
2023-12-12 20:10:13 +0100

    Styling and weight fixed

--- src/Components/Poke_cards.jsx
+++ src/Components/Poke_cards.jsx
@@ -1,22 +1,41 @@
 
 
 const PokeCard = ({id, img, name, tipo, height, weight, abilities,}) =>{
+
+    const PesoReal = (weight / 10).toFixed(1)
+
     return(
-        <div className="justify-center align-middle bg-sky-950 w-full h-96 rounded-md backdrop-blur-md backdrop-filter bg-opacity-30 shadow shadow-yellow-400">
-            <div className="m-1 flex-col justify-center align-middle">
-                <span className="text-amber-200 font-bold text-center p-5 text-3xl">#{id}</span>
-                    <div className="shadow flex align-middle justify-end m-auto w-36 h-26 opacity-80 hover:opacity-100">
-                        <img className="object-cover w-full h-full transition-all" src={img} alt={id}></img>
+        <div className="justify-center align-middle bg-sky-950 w-full h-full rounded-md backdrop-blur-xl backdrop-filter bg-opacity-30 shadow-md shadow-zinc-900">
+            <div className="">
+                
+                    <div className="grid grid-cols-2 m-auto opacity-80 hover:opacity-100 transition-all">
+                        
+                        <h1 className="text-amber-50 w-full h-full opacity-20 p-3 text-9xl">#{id}</h1>
+                        
+                        
+                        <img className="object-contain flex" src={img} alt={id}></img>
+                        
+                        
                     </div>
                 
                 <h1 className=" text-yellow-100 font-extrabold pt-4 capitalize flex justify-center align-middle">{name}</h1>
-                <span className="text-yellow-100 capitalize p-3 font-bold">Type: {tipo}</span>
+                
             </div>
 
             <div className="text-yellow-50 p-3 flex-col">
-                <p className="p-2">Height: {height}</p>
-                <p className="p-2">Weight: {weight}</p>
+            <span className="text-yellow-100 capitalize p-2 font-bold">Type: {tipo}</span>
+
                 <p className="p-2 capitalize">Ability: {abilities}</p>
+                
+                <p className="p-2">Height:
+                <progress value={height / 10} max={20} />
+                 {height}
+                 </p>
+                <p className="p-2">Weight:
+                <progress value={PesoReal} max={999.9}/>
+                 {PesoReal}
+                 </p>
+                
             </div>
 
         </div>
